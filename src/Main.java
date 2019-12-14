d7cfffb31a000a566107ceb4670e975501ab0386
kazuw0401 <kazumasaw0401@gmail.com>
2019-12-14 14:32:12 +0900

    first commit!

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 12;
+        int b = 5;
+        System.out.println(a * b);
+        System.out.println(a / b);
+        System.out.println(a % b);
+    }
+}
