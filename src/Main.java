1c582de26b8ed0d305a4d8170f0d551dd441fab2
kazuw0401 <kazumasaw0401@gmail.com>
2019-12-28 12:49:30 +0900

    練習13終了

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,9 @@ import practice10.Wizard;
 import practice11.PoisonMatango;
 import practice6.First;
 import practice8.Cleric;
+import practice13.A;
+import practice13.B;
+import practice13.Y;
 
 import java.util.Random;
 
@@ -45,9 +48,9 @@ public class Main {
 //    }
 
 //    練習9
-    public static void main(String[] args) {
+//    public static void main(String[] args) {
 //        コンストラクタ宣言をしているので、インスタンス作成時にname,hp,mpの引数を設定
-        Cleric c = new Cleric("Tom", 100, 10);
+//        Cleric c = new Cleric("Tom", 100, 10);
 //        c.name = "Tom";
 //        c.hp = 100;
 //        c.mp = 10;
@@ -55,27 +58,38 @@ public class Main {
 //        c.selfRecover();
 //        c.pray(new Random().nextInt(3));
 
-        Wizard wiz = new Wizard();
-        wiz.setName("Mary");
-        wiz.setHp(40);
-        wiz.setMp(20);
+//        Wizard wiz = new Wizard();
+//        wiz.setName("Mary");
+//        wiz.setHp(40);
+//        wiz.setMp(20);
+//
+//        Wand w = new Wand();
+//        w.setName("水晶の杖");
+//        w.setPower(5.0);
+//
+//        System.out.println("司祭の名前：" + c.name);
+//        System.out.println("司祭のHP：" + c.hp);
+//        System.out.println("司祭のMP：" + c.mp);
+//
+//        System.out.println("魔法使いの名前：" + wiz.getName());
+//        System.out.println("魔法使いのHP：" + wiz.getHp());
+//        System.out.println("魔法使いのMP：" + wiz.getMp());
+//        System.out.println("杖の名前：" + w.getName());
+//        System.out.println("杖の増幅：" + w.getPower());
+//
+//        PoisonMatango pm = new PoisonMatango('A');
+//        pm.attack(c);
 
-        Wand w = new Wand();
-        w.setName("水晶の杖");
-        w.setPower(5.0);
+//    }
 
-        System.out.println("司祭の名前：" + c.name);
-        System.out.println("司祭のHP：" + c.hp);
-        System.out.println("司祭のMP：" + c.mp);
-
-        System.out.println("魔法使いの名前：" + wiz.getName());
-        System.out.println("魔法使いのHP：" + wiz.getHp());
-        System.out.println("魔法使いのMP：" + wiz.getMp());
-        System.out.println("杖の名前：" + w.getName());
-        System.out.println("杖の増幅：" + w.getPower());
-
-        PoisonMatango pm = new PoisonMatango('A');
-        pm.attack(c);
-
+//    練習13
+    public static void main(String[] args) {
+        Y[] array = new Y[2];
+        array[0] = new A();
+        array[1] = new B();
+        for (Y y : array) {
+            y.a();
+            y.b();
+        }
     }
 }
